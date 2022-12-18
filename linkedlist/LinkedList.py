0a77fc54eb6f94499b5d1ac7d28df0d0c9c6c4e9
Abhijeet Chakravorty <abhijeet@moneytor.in>
2022-12-18 13:43:55 +0530

    updated code for multi level linked list

--- linkedlist/LinkedList.py
+++ linkedlist/LinkedList.py
@@ -4,11 +4,18 @@ class Node:
         self.next = next
         self.prev = prev
 
+class ChildNode:
+    def __init__(self, val=None, next=None, prev=None, child=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+        self.child = child
+
 class MyLinkedList:      
     def __init__(self):
         self.head = None
     
-    def createDynamicList(self, arr):
+    def createDynamicSinglyList(self, arr):
         self.head = Node(arr[0])
         temp = self.head
         i = 1
@@ -18,11 +25,33 @@ class MyLinkedList:
             temp = temp.next
             
         return self.head
+    
+    def createDynamicDoublyList(self, arr):
+        self.head = Node(arr[0]) #set first head for returning the head 
+        # 1 --> 2 --> 3
+        temp = self.head #set head to a temp variable for the iteration
+        i = 1 # set a counter for iterating over an array
+        while(i<len(arr)):
+            temp.next = Node(arr[i])
+            i += 1
+            temp.next.prev = temp
+            temp = temp.next
+        
+        return self.head
                 
     def printList(self, head):
         temp = head
         while(temp):
             print(temp.val)
+            temp = temp.next
+    
+    def printDoublyList(self, head):
+        temp = head
+        while(temp):
+            if (temp.prev):
+                print(temp.val, temp.prev.val)
+            else:
+                print(temp.val, None)
             temp = temp.next
 
     def reverseList(self, head):
@@ -66,7 +95,6 @@ class MyLinkedList:
         return out.next
     
     def addTwoNumbers(self, l1, l2):
-        print("We are going full ballistic no more cheating and no more code checking USE YOUR BRAIN")
         result_tail = Node(0)
         result = result_tail
         carry = 0
@@ -84,6 +112,9 @@ class MyLinkedList:
             
         return result_tail.next
     
+    def flattenList(self, head):
+        self.printList(head)
+    
     
     
     
@@ -92,16 +123,34 @@ class MyLinkedList:
 arr = [1, 3, 5, 7, 9]
 arr2 = [2, 4, 6, 8, 10]
 head = MyLinkedList()
-list = head.createDynamicList(arr)
+# list = head.createDynamicSinglyList(arr)
 # print(head.isPalindrome(list))
 # newList = head.reverseList(list)  # reverse List
-print("//////////////////////")
+# print("//////////////////////")
 # print(head.isPalindrome(list))
 
 
-list2 = head.createDynamicList(arr2)
+# list2 = head.createDynamicSinglyList(arr2)
 # head.printList(list)
 # head.printList(list2)
 # head.printList(list2)
-mergedList = head.mergeTwoSortedList(list, list2)
-head.printList(mergedList)
+# mergedList = head.mergeTwoSortedList(list, list2)
+# head.printList(mergedList)
+# print("//////////////////////")
+# print("//////////////////////")
+# sumList = head.addTwoNumbers(list, list2)
+# head.printList(sumList)
+
+
+# doublyList = head.createDynamicDoublyList(arr)
+# head.printDoublyList(doublyList)
+
+
+head.val = ChildNode(1)
+head.next = ChildNode(2)
+head.next.next = ChildNode(3)
+head.next.next.next = ChildNode(4)
+head.next.next.next.next = ChildNode(5)
+head.next.next.next.next.next = ChildNode(6)
+head.next.next.child = ChildNode(7)
+head.next.next.child.next = ChildNode(8)
